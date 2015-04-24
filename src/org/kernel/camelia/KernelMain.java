8a54262e2ad9d4d1913955707f2985f63f57fb71
Lopez Jimmy <jimmy.lopez@etud.univ-montp2.fr>
2015-04-24 13:36:17 +0200

    Add main

---
+++ src/org/kernel/camelia/KernelMain.java
@@ -0,0 +1,12 @@
+package org.kernel.camelia;
+
+/**
+ * Created by jimmy on 24/04/15.
+ */
+public class KernelMain {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Kernel Camelia");
+
+    }
+}
